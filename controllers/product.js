4cd0332ad013902afeb1ae4d077e977bbdf571e6
Vlad <paliy.work@gmail.com>
2019-12-17 14:35:48 +0200

    Added CRUD update.

--- controllers/product.js
+++ controllers/product.js
@@ -86,3 +86,51 @@ exports.remove = (req, res) => {
   }) 
 
 }
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+
+  form.keepExtensions = true;
+  form.parse(req , (err, fields, files)=>{
+    if (err) {
+      return res.status(400).json({
+        error: 'Image not found'
+      })
+    }
+
+    // check for all fields
+    const {name, description, price, category, quantity, shipping } = fields;
+
+    if (!name || !description || !price || !category ||  !quantity || !shipping ) {
+      return res.status(400).json({
+        error: 'All fields are required'
+      })
+    }
+
+    let product = req.product;
+    product = _.extend(product, fields);
+
+    if (files.photo) {
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+
+    if(files.photo.size > 1000000) {
+      return res.status(400).json({
+        error: 'Image should be less than 1mb'
+      })
+    }
+
+    product.save((err, result) => {
+      if(err) {
+        return res.status(400).json({
+          error: errorHandler(err)
+        })
+      }
+
+      res.json(result);
+
+    });
+
+  })
+}
